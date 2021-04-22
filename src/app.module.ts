03998475521299a70fc538f42fe4134688a56267
NastiiaV <awerga13@gmail.com>
2021-04-22 17:54:40 +0300

    added procfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-//import { join } from 'path';
-//import { AppController } from './app.controller';
 import { AppGateway } from './app.gateway';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // ServeStaticModule.forRoot({
-    //   rootPath: join(__dirname, '..', 'client'),
-    //   exclude: ['/api*'],
-    // }),
+    //ConfigModule.forRoot()
   ],
   //controllers: [AppController],
   providers: [AppGateway],
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   //app.setGlobalPrefix('api');
   app.useStaticAssets(join(__dirname, '..', 'client'));
-  await app.listen(3000);
+  await app.listen( process.env.PORT || 3000);
 }
 bootstrap();
